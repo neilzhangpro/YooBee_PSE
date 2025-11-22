23036057ea0b67db21068a71fa8111027841b290
neilzhangpro <tomiezhangnz@gmail.com>
2025-11-23 08:54:28 +1300

    add commits

--- DataTypes.py
+++ DataTypes.py
@@ -3,25 +3,27 @@ Python Data Types
 This file is used to show the data types in Python.
 @author: Yaohui Zhang @tomie
 """
-# String
+# String type data
 Name = "John Doe"
 
-#Integer
+#Integer type data, can only be integer
 Age = 28
 
-#List
+#List type data, can be a list of any data 
 Skills = ["Python", "SQL", "Power BI"]
 
-#Tuple
+#Tuple type data , can be a list of any data but cannot be changed
 Education = ("BSc Computer Science", 2020)
 
-#Dictionary
+#Dictionary type data, can be a dictionary of any data
 Contact_details = {
     "email": "tomieweb@gmail.com",
     "phone": "+1234567890",
 }
 
-#Set
+#Set type data, can be a set of any data but should be unique
+#This is a set of certifications, which are unique and cannot be duplicated
+# it is will be removed repeated items automatically
 Certifications = {"Azure", "AWS", "Azure"}
 
 #print
